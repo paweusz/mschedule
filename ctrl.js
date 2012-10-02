38889be2ca68739c31fa60ac10b64e56c30feb1b
Pawel Pelczar <paweusz@gmail.com>
2012-10-02 18:38:20 +0200

    Displaying lessons for given weekday

--- ctrl.js
+++ ctrl.js
@@ -12,16 +12,29 @@ msched.controller('ScheduleCtrl', ['$scope', function ($scope) {
     scope.current = weekdays[scope.index].name;
     scope.next = weekdays[scope.index + 1 < weekdays.length ? scope.index + 1 : 0].name;
   };
-  updateLabels($scope);
+  
+  function updateLessons(scope) {
+    scope.lessons = _.map(msched.lessons[scope.index], function(l) {
+      return l != null ? msched.subjects[l] : null;
+    });
+  };
+  
+  function update(scope) {
+    updateLabels(scope);
+    updateLessons(scope);
+  };
   
   $scope.backward = function() {
     this.index = this.index - 1 >= 0 ? this.index - 1 : msched.weekdays.length - 1;
-    updateLabels(this);
+    update(this);
   };
   
   $scope.forward = function() {
     this.index = this.index + 1 < msched.weekdays.length ? this.index + 1 : 0;
-    updateLabels(this);
+    update(this);
   };
+  
+  update($scope);
+  
 }]);
 
--- domain.js
+++ domain.js
@@ -3,11 +3,66 @@
 var msched = angular.module('MSchedule');
 
 msched.weekdays = [
-  {id: "Mon", name: "Monday"},
-  {id: "Tue", name: "Tuesday"},
-  {id: "Wed", name: "Wednesday"},
-  {id: "Thu", name: "Thursday"},
-  {id: "Fri", name: "Friday"},
+  {id: "Mon", name: "Poniedziałek"},
+  {id: "Tue", name: "Wtorek"},
+  {id: "Wed", name: "Środa"},
+  {id: "Thu", name: "Czwartek"},
+  {id: "Fri", name: "Piątek"}
 ];
 
+msched.hmToDate = function(hour, minute) {
+  return new Date(0, 0, 1, hour, minute);
+};
+
+msched.Timeslot = function(startHour, startMinute, endHour, endMinute) {
+  this.start = msched.hmToDate(startHour, startMinute);
+  this.end = msched.hmToDate(endHour, endMinute);
+}
+
+msched.Timeslot.prototype = {
+
+  getStartHour: function() {
+    return this.start.getHours();
+  },
+  
+  getStartMinute: function() {
+    return this.start.getMinutes();
+  },
+  
+  getEndHour: function() {
+    return this.end.getHours();
+  },
+  
+  getEndMinute: function() {
+    return this.end.getMinutes();
+  }
+  
+};
+
+msched.timeslots = [
+  {id: 1, timeslot: new msched.Timeslot(8, 0, 8, 45)},
+  {id: 2, timeslot: new msched.Timeslot(8, 50, 9, 35)},
+  {id: 3, timeslot: new msched.Timeslot(9, 45, 10, 30)},
+  {id: 4, timeslot: new msched.Timeslot(10, 40, 11, 25)},
+  {id: 5, timeslot: new msched.Timeslot(11, 45, 12, 30)},
+  {id: 6, timeslot: new msched.Timeslot(12, 45, 13, 30)},
+  {id: 7, timeslot: new msched.Timeslot(13, 35, 14, 20)},
+  {id: 8, timeslot: new msched.Timeslot(14, 25, 15, 10)}
+];
+
+msched.subjects = {
+  ew: "Edukacja wczesnoszkolna",
+  inf: "Zajęcia komputerowe",
+  ewsg: "Wychowanie fizyczne",
+  ang: "Język angielski",
+  rel: "Religia"
+};
+
+msched.lessons = [
+  ["ew", "ew", "ew", "inf"],
+  ["ew", "ewsg", "ang", "ew", "ew"],
+  ["ew", "ew", "ew", "rel", "ang"],
+  ["ew", "ewsg", "ew", "ew", "ew"],
+  [null, null, "rel", "ew", "ew", "ew"]
+];
 
