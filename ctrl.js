c7e8d31029d69574c7a47b02940c27899d2304b0
Pawel Pelczar <paweusz@gmail.com>
2012-10-02 16:30:02 +0200

    Handling weekdays traversing

--- ctrl.js
+++ ctrl.js
@@ -1,9 +1,27 @@
 "use strict";
 
-var msched = angular.module('MSchedule', []);
+var msched = angular.module('MSchedule');
 
 msched.controller('ScheduleCtrl', ['$scope', function ($scope) {
-  $scope.previous = "Friday";
-  $scope.next = "Tuesday";
+
+  $scope.index = 0;
+
+  function updateLabels(scope) {
+    var weekdays = msched.weekdays;
+    scope.previous = weekdays[scope.index > 0 ? scope.index - 1 : weekdays.length - 1].name;
+    scope.current = weekdays[scope.index].name;
+    scope.next = weekdays[scope.index + 1 < weekdays.length ? scope.index + 1 : 0].name;
+  };
+  updateLabels($scope);
+  
+  $scope.backward = function() {
+    this.index = this.index - 1 >= 0 ? this.index - 1 : msched.weekdays.length - 1;
+    updateLabels(this);
+  };
+  
+  $scope.forward = function() {
+    this.index = this.index + 1 < msched.weekdays.length ? this.index + 1 : 0;
+    updateLabels(this);
+  };
 }]);
 
---
+++ domain.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var msched = angular.module('MSchedule');
+
+msched.weekdays = [
+  {id: "Mon", name: "Monday"},
+  {id: "Tue", name: "Tuesday"},
+  {id: "Wed", name: "Wednesday"},
+  {id: "Thu", name: "Thursday"},
+  {id: "Fri", name: "Friday"},
+];
+
+
